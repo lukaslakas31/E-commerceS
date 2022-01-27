4c31a7a461d4ffbc04c207ddb33bcb51c5ebef23
lukaslakas31 <lukejuniel@gmail.com>
2022-01-28 00:42:26 +0800

    gumana na

---
+++ frontend/src/data.js
@@ -0,0 +1,70 @@
+const data = {
+    products: [{
+            _id: "1",
+            name: "Patriarch of the Winds Rooster 2021",
+            category: "Animals",
+            image: "/pictures/m1.png",
+            price: '$600',
+            brand: "Nike",
+            rating: 4.5,
+            numReview: 10,
+            description: "High quality product"
+        },
+        {
+            _id: "2",
+            name: "Captain of Dwarves Rooster 2022",
+            category: "Animals",
+            image: "/pictures/m2.png",
+            price: '$600',
+            brand: "Nike",
+            rating: 4.5,
+            numReview: 10,
+            description: "High quality product"
+        },
+        {
+            _id: "3",
+            name: "Minister of the West 1.0",
+            category: "Shirts",
+            image: "/pictures/m3.png",
+            price: '$600',
+            brand: "Nike",
+            rating: 4.5,
+            numReview: 10,
+            description: "High quality product"
+        },
+        {
+            _id: "4",
+            name: "Assistant of Vigor version 11.2 hotfix",
+            category: "Shirts",
+            image: "/pictures/m4.png",
+            price: '$600',
+            brand: "Nike",
+            rating: 4.5,
+            numReview: 10,
+            description: "High quality product"
+        },
+        {
+            _id: "5",
+            name: "Overlord of Sports Father",
+            category: "Shirts",
+            image: "/pictures/m5.png",
+            price: '$600',
+            brand: "Animal",
+            rating: 7,
+            numReview: 10,
+            description: "High quality derby"
+        },
+        {
+            _id: "6",
+            name: "High Priest of Sanctity",
+            category: "Shirts",
+            image: "/pictures/m6.png",
+            price: '₿ 2',
+            brand: "Animal",
+            rating: 100,
+            numReview: 10,
+            description: "High quality derby"
+        }
+    ]
+}
+export default data;
